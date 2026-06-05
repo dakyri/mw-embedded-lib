eb12fd5dd29e50a710800af03b5070796f83c32a
dakyri <dakyri@gmail.com>
2026-06-06 00:38:35 +0200

    pi i2c via wiring

--- pi2c/pi2c.h
+++ pi2c/pi2c.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#ifdef HAS_WIRING_PI
 #include <wiringpi.h>
 #include <wiringPiI2C.h>
+#endif
 
 /*!
  * simple wrapper around wiring pi i2c so we can use the arduino code on raspberry pi
@@ -11,17 +13,21 @@ public:
 	Pi2c(const uint8_t _address): address(_address) {}
 
 	~Pi2c() {
+#ifdef HAS_WIRING_PI
 		if (dfd >= 0) {
 			wiringPiI2CClose(dfd);
 		}
+#endif
 	}
 
 	bool begin() {
+#ifdef HAS_WIRING_PI
 		if (!hasBegun) {
 			wiringPiSetup();
 			hasBegun = true;
 		}
 		dfd = wiringPiI2CSetup(address);
+#endif
 		return dfd >= 0;
 	}
 
@@ -29,14 +35,30 @@ public:
 	/*!
 	 *  Write byte to register
 	 */
-	void writeByte(uint8_t reg, uint8_t value) { wiringPiI2CWriteReg8(dfd, reg, value); }
-	void writeByte(uint8_t value) {	wiringPiI2CWrite(dfd, value); }
+	void writeByte(uint8_t reg, uint8_t value) {
+#ifdef HAS_WIRING_PI
+		wiringPiI2CWriteReg8(dfd, reg, value);
+#endif
+	}
+	void writeByte(uint8_t value) {
+#ifdef HAS_WIRING_PI
+		wiringPiI2CWrite(dfd, value);
+#endif
+	}
 
 	/*!
 	 *  Read byte from register
 	 */
-	uint8_t readByte(uint8_t reg) { return wiringPiI2CRead8(dfd, reg); }
-	uint8_t readByte() { return wiringPiI2CRead(dfd); }
+	uint8_t readByte(uint8_t reg) {
+#ifdef HAS_WIRING_PI
+		return wiringPiI2CRead8(dfd, reg);
+#endif
+}
+	uint8_t readByte() {
+#ifdef HAS_WIRING_PI
+		return wiringPiI2CRead(dfd);
+#endif
+	}
 
 	/*!
 	 * Read/write whole heap of stuff!
@@ -44,8 +66,16 @@ public:
 	 * \param n number of bytes to read
 	 * \param buf where to sticl the results
 	 */
-	int readBuf(uint8_t reg, size_t n, uint8_t *buf) { return wiringPiI2CReadBlockData (fd, reg, *buf, n); }
-	int writeBuf(uint8_t reg, size_t n, uint8_t *buf) { return wiringPiI2CWriteBlockData (fd, reg, *buf, n); }
+	int readBuf(uint8_t reg, size_t n, uint8_t *buf) {
+#ifdef HAS_WIRING_PI
+		return wiringPiI2CReadBlockData (fd, reg, *buf, n);
+#endif
+	}
+	int writeBuf(uint8_t reg, size_t n, uint8_t *buf) {
+#ifdef HAS_WIRING_PI
+		return wiringPiI2CWriteBlockData (fd, reg, *buf, n); 
+#endif
+	}
 
 	/*
 	uint8_t fastReadByte(uint8_t reg);
@@ -62,7 +92,7 @@ public:
 	const int dfd = -1;
 private:
 	static bool hasBegun;
-}
+};
 
 
 
@@ -124,5 +154,3 @@ bool Pi2c::readBit(uint8_t reg, uint8_t pos)
 	return ((value >> pos) & 1);
 }
 */
-
-
